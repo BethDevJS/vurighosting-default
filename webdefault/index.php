1948fd6dc3c1a886180859e059bb96ece03179d3
Beth <70341936+BethDevJS@users.noreply.github.com>
2023-02-06 21:39:32 +0000

    Update index.php
    First version of default webpage.

--- webdefault/index.php
+++ webdefault/index.php
@@ -1,1 +1,45 @@
-Development in progress
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Vurig Hosting - Nginx Hosting</title>
+    <link rel="icon" type="image/x-icon" href="https://img.yxri.dev/9NhUS.png"/>
+</head>
+<body>
+    <img src="https://img.yxri.dev/8c0FM.png" width="250px"/>
+    <h2>
+        Thanks for your order!
+        <br/>
+        Your web server is up and running!
+        <br/>
+        <br/>
+        To get started head over to the file editor and open up the "webroot" folder to start building your website!
+    </h2>
+</body>
+</html>
+
+<style>
+* {
+    font-family: 'Lexend Deca', sans-serif; 
+    color: white;
+    background-color: #11111d;
+}
+
+h2 {
+    margin: 0;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    text-align: center;
+}
+
+img {
+    position: absolute;
+    top: 25%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+}
+</style>
